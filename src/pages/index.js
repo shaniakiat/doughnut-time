3069b76dcd60096d97c2d7de17ba7bf7165b5b8c
shaniakiat <kiat.shania@gmail.com>
2019-07-10 12:15:47 -0400

    logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,16 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { FaShoppingCart } from "react-icons/fa"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <p>
+      <FaShoppingCart />
+    </p>
   </Layout>
 )
 
