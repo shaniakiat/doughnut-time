81b8a9768434f162b04e2f9f65c558cf977b821b
shaniakiat <kiat.shania@gmail.com>
2019-07-10 23:45:43 -0400

    navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,14 @@ import { useStaticQuery, graphql } from "gatsby"
 import "./bootstrap.min.css"
 import "./layout.css"
 
-const Layout = ({ children }) => <>{children}</>
+import Navbar from "./Globals/Navbar"
+
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,13 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { FaShoppingCart } from "react-icons/fa"
+// import { FaShoppingCart } from "react-icons/fa"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>
-      <FaShoppingCart />
-    </p>
   </Layout>
 )
 
