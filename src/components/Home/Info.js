df9ce70684bc190c27d7e971b584c09d291cdd72
shaniakiat <kiat.shania@gmail.com>
2019-07-18 18:09:49 -0400

    styling

--- src/components/Home/Info.js
+++ src/components/Home/Info.js
@@ -4,18 +4,22 @@ import Title from "../Globals/Title"
 
 export default function Info() {
   return (
-    <section className="py-5">
+    <section className="info py-5">
       <div className="container">
         <Title title="our story"></Title>
         <div className="row">
-          <div className="col-10 col-sm-8 mx-auto text-center">
-            <p className="lead text-muted mb-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              illo distinctio quaerat facere odit sit quod adipisci, quam
-              tempore harum saepe. Magnam minus alias tenetur tempore porro
-              earum laboriosam excepturi, corporis iusto facilis, doloribus qui
-              nobis illum eius quibusdam non esse eum reiciendis nostrum
-              veritatis quas iure autem. Vitae, iure?
+          <div className="col-10 col-sm-10 mx-auto text-center">
+            <p className="lead mb-5">
+              Donut sweet cookie cake biscuit pastry ipsum apple vanilla
+              sprinkles cake apple sweet pastry sweet cookie marzipan apple
+              cream jelly. Cookie donut cookie candy sprinkles dolor sweet
+              muffin cookie muffin orange apple marzipan jelly tiramisu
+              croissant cookie tiramisu marzipan croissant. Sprinkles muffin
+              cake biscuit cookie sweet vanilla orange jelly pastry vanilla
+              apple dolor sweet dolor sprinkles cookie vanilla vanilla ipsum.
+              Jelly apple ipsum dolor cookie marzipan marzipan tiramisu
+              sprinkles sprinkles marzipan apple donut cream pastry marzipan
+              jelly biscuit lollipop muffin.
             </p>
             <Link to="/about/">
               <button className="btn text-uppercase">about page</button>
