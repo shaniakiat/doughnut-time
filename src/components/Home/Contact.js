17594fbc36da19af7bf438543876fc96701077bd
shaniakiat <kiat.shania@gmail.com>
2019-07-19 20:43:44 -0400

    contact and styling

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -1,5 +1,56 @@
 import React from "react"
+import Title from "../Globals/Title"
 
 export default function Contact() {
-  return <div>Hello</div>
+  return (
+    <section className="contact py-5">
+      <Title title="contact us" />
+      <div className="row">
+        <div className="col-10 col-sm-8 col-md-6 mx-auto">
+          <form
+            action="https://formspree.io/kiat.shania@gmail.com"
+            method="POST"
+          >
+            {/* name */}
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                id="name"
+                placeholder="john smith"
+              />
+            </div>
+            {/* email */}
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                id="email"
+                placeholder="email@email.com"
+              />
+            </div>
+            {/* description */}
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <textarea
+                name="description"
+                id="description"
+                className="form-control"
+                rows="5"
+                placeholder="you description here..."
+              />
+            </div>
+            {/* submit */}
+            <button type="submit" className="btn btn-block text-uppercase mt-5">
+              submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  )
 }
